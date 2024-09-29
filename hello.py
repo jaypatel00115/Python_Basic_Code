5de9a6c4d15c41d5f5ff02265908850b96e1d151
Jay Patel <126940694+jaypatel00115@users.noreply.github.com>
2024-09-29 18:00:53 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,28 @@
+
+
+'''print("hello") # with double quatation
+
+print('hello') # with single quatation
+
+v=2
+'''
+'''
+x,z=2,2
+y=3.2
+c=x+z
+ans = "sum"
+print(c)
+print(ans)
+'''
+
+'''
+name='jay patel'
+print(name)
+print(name[0:3])
+'''
+
+#n=['jay',2525] ; print (n) ; print (n[0])
+
+l=range (3)
+for i in l:
+    print(i)
---
+++ if_else.py
@@ -0,0 +1,15 @@
+
+
+# Use of if ... elseif in Python 
+
+opt=input('Enter the choice,like square : 1 and rectangular : 2, so opt=')
+if opt=='1':
+ print('you selected to find the area for square shape')
+ a=float(input('enter one side of square :'))
+ 
+ print('your square area is:',a*a) 
+elif opt=='2':
+ print('you selected to find the area for rectangale shape')
+ a=int(input('enter one side of rec. :'))
+ b=int(input('enter second side of rec. :'))
+ print('your rectangle area is:',a*b) 
