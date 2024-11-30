1adc976ccb7dd91fda4367ffecb3447982ce90fe
Jay Patel <126940694+jaypatel00115@users.noreply.github.com>
2024-11-30 20:00:38 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,28 +1,20 @@
 
-
-'''print("hello") # with double quatation
-
-print('hello') # with single quatation
-
-v=2
+# Different printing functions used in the Python
 '''
+# with double quotation
+print("hello") 
+
+# with a single quotation
+print('hello') 
+'''
+
 '''
 x,z=2,2
 y=3.2
 c=x+z
-ans = "sum"
+ans=c
 print(c)
 print(ans)
 '''
 
-'''
-name='jay patel'
-print(name)
-print(name[0:3])
-'''
 
-#n=['jay',2525] ; print (n) ; print (n[0])
-
-l=range (3)
-for i in l:
-    print(i)
